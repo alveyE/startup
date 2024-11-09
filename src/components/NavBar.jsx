3259f3bfba8bb756e70a35eb7d86f3d39f6ba4a8
alveyE <gcbdalvey@gmail.com>
2024-11-09 14:03:30 -0700

    reorganize

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+  return (
+    <div className="navbar">
+      <a className="active" href="index.html">
+        Aisle Hawk
+      </a>
+      <a href="list.html">My List</a>
+      <a href="sales.html">Sales</a>
+    </div>
+  );
+}
+
+export default Navbar;
