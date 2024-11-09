18c380f1902d9cf20978d6937d6f0faae977a0bf
alveyE <gcbdalvey@gmail.com>
2024-11-09 14:23:18 -0700

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>&copy; Ethan Alvey</p>
+      <a href="https://github.com/alveyE/startup">Github Repo</a>
+    </footer>
+  );
+}
+
+export default Footer;
