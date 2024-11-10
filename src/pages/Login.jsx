71fd315e4affc493810df2e6bdd91c9d67f02ab2
alveyE <gcbdalvey@gmail.com>
2024-11-09 18:23:50 -0700

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,32 @@
+import { useNavigate } from "react-router";
+import Footer from "../components/Footer";
+import Navbar from "../components/NavBar";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Login Page</p>
-    </div>
+    <>
+      <Navbar />
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+        <br />
+        <br />
+        <button className="button">Login</button>
+        <button className="button">Create Account</button>
+      </form>
+      <Footer />
+    </>
   );
 }
 
