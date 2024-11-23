6a92f2d1e698bfff7a0deac839a072a0e09645ea
alveyE <gcbdalvey@gmail.com>
2024-11-22 21:35:35 -0700

    modify index to use database

--- service/index.js
+++ service/index.js
@@ -4,6 +4,11 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("csv-parser");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const DB = require("./database.js");
+
+const authCookieName = "token";
 
 let users = {};
 let prices = [];
@@ -14,49 +19,49 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static("public"));
+
+app.set("trust proxy", true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
+// CreateAuth token for a new user
 apiRouter.post("/auth/create", async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
+  if (await DB.getUser(req.body.email)) {
     res.status(409).send({ msg: "Existing user" });
   } else {
-    const user = {
-      email: req.body.email,
-      password: req.body.password,
-      token: uuidv4(),
-    };
-    users[user.email] = user;
+    const user = await DB.createUser(req.body.email, req.body.password);
 
-    res.send({ token: user.token });
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
   }
 });
 
-// GetAuth login an existing user
+// GetAuth token for the provided credentials
 apiRouter.post("/auth/login", async (req, res) => {
-  const user = users[req.body.email];
+  const user = await DB.getUser(req.body.email);
   if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuidv4();
-      res.send({ token: user.token });
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
       return;
     }
   }
   res.status(401).send({ msg: "Unauthorized" });
 });
 
-// DeleteAuth logout a user
-apiRouter.delete("/auth/logout", (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
+// DeleteAuth token if stored in cookie
+apiRouter.delete("/auth/logout", (_req, res) => {
+  res.clearCookie(authCookieName);
   res.status(204).end();
 });
 
@@ -70,11 +75,39 @@ apiRouter.get("/prices", (_req, res) => {
   res.send(prices);
 });
 
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile("index.html", { root: "public" });
 });
 
-app.listen(port, () => {
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+}
+
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
